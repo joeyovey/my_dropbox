4d4d0cb1b9e0e57d0eef3b8ebeaea33f95ff61a3
hr1 <hr1@outsource.ng>
2024-10-09 09:34:55 +0100

    Reconfigured the firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore, collection, serverTimestamp } from "firebase/firestore"
-import { getStorage } from "firebase/storage"
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+import "firebase/compat/storage"
 
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyArQtdX1SmbyjW--DsKC9BZ3nFWPJrYRAI",
   authDomain: "drop-box-905b1.firebaseapp.com",
   projectId: "drop-box-905b1",
@@ -11,19 +11,19 @@ const firebaseConfig = {
   messagingSenderId: "1073796093455",
   appId: "1:1073796093455:web:dcba31fce0c052dec1757a",
   measurementId: "G-SQ5KWSMWW4"
-};
+})
 
-const app = initializeApp(firebaseConfig)
+const firestore = app.firestore()
 
-export const auth = getAuth(app)
+
 export const db = {
-  folders: collection(getFirestore(app), "folders"),
-  files: collection(getFirestore(app), "files"),
+  folders: firestore.collection("folders"),
+  files: firestore.collection("files"),
   formatDoc: doc => {
     return { id: doc.id, ...doc.data() }
   },
-  getCurrentTimestamp: serverTimestamp
+  getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp,
 }
-export const storage = getStorage(app)
-
+export const storage = app.storage()
+export const auth = app.auth()
 export default app
