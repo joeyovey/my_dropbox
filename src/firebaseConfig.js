70b2988240be4787ba0fbb7c269d633b598b88fd
hr1 <hr1@outsource.ng>
2024-10-11 10:23:06 +0100

    Functionto delete a folder

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,7 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
-import "firebase/compat/storage"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const app = firebase.initializeApp({
   apiKey: "AIzaSyArQtdX1SmbyjW--DsKC9BZ3nFWPJrYRAI",
@@ -11,19 +11,24 @@ const app = firebase.initializeApp({
   messagingSenderId: "1073796093455",
   appId: "1:1073796093455:web:dcba31fce0c052dec1757a",
   measurementId: "G-SQ5KWSMWW4"
-})
+});
 
-const firestore = app.firestore()
-
+const firestore = app.firestore();
 
 export const db = {
   folders: firestore.collection("folders"),
   files: firestore.collection("files"),
-  formatDoc: doc => {
-    return { id: doc.id, ...doc.data() }
+  formatDoc: (doc) => {
+    return { id: doc.id, ...doc.data() };
   },
   getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp,
+};
+
+// Function to delete a folder from Firebase
+export function deleteFolder(folderId) {
+  return db.folders.doc(folderId).delete();
 }
-export const storage = app.storage()
-export const auth = app.auth()
-export default app
+
+export const storage = app.storage();
+export const auth = app.auth();
+export default app;
